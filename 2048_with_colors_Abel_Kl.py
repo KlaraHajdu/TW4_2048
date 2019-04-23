7499b45e195ed7654b34decf3cf043a9fd59a57a
Klara Hajdu <hajduklara78@gmail.com>
2019-04-23 14:59:45 +0200

    Changed to orange

--- 2048_with_colors_Abel_Kl.py
+++ 2048_with_colors_Abel_Kl.py
@@ -305,7 +305,7 @@ def keyboard_inputs(stdscr):
 def main(stdscr):
     # init color-pairs
 
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(1, curses.COLOR_ORANGE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
@@ -334,9 +334,9 @@ def main(stdscr):
     while True:
         stdscr.clear()
         stdscr.addstr("Game over")
-        stdscr.refresh        
+        stdscr.refresh
         if stdscr.getch() == ord('q'):
-                break
+            break
 
 
 # wrap it so it doesn't mess with terminal settings while debugging
