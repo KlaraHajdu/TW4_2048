f0994bc94cf406d0d9f0bffa492a1ec77d5ed132
Klara Hajdu <hajduklara78@gmail.com>
2019-04-24 11:08:20 +0200

    Red

--- 2048_with_colors_Abel_Kl.py
+++ 2048_with_colors_Abel_Kl.py
@@ -305,7 +305,7 @@ def keyboard_inputs(stdscr):
 def main(stdscr):
     # init color-pairs
 
-    curses.init_pair(1, curses.COLOR_ORANGE, curses.COLOR_BLACK)
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_YELLOW, curses.COLOR_BLACK)
     curses.init_pair(4, curses.COLOR_BLUE, curses.COLOR_BLACK)
